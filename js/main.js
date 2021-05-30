6d39b9aabbe1dd069752d09e35f45b1dee30700a
sergdeveloper <serg.developer@yandex.ru>
2021-05-30 19:51:40 +0300

    Ошибки линтера и более корректные названия переменных

--- js/main.js
+++ js/main.js
@@ -1,25 +1,25 @@
 //случайное число из диапазона
 
-const getRandom = function (from, to) {
-    if (from > 0 && to > 0 && from < to) {
-      return Math.floor(Math.random() * (to - from + 1)) + from;
-    }
-    else {
-      return 'Минимальное число не может быть отрицательным и больше макисмального';
-    }
-  };
-  
-  getRandom (100, 90);
-  
-  //проверка длины комментария
-  
-  const getLength = function (field, amount) {
-    if (field.length <= amount) {
-      return 'Комментарий отправлен';
-    }
-    else {
-      return 'Превышен лимит символов';
-    }
-  };
-  
-  getLength(100, 140);
+const getRandom = function (minimal, maximal) {
+  if (minimal > 0 && maximal > 0 && minimal < maximal) {
+    return Math.floor(Math.random() * (maximal - minimal + 1)) + minimal;
+  }
+  else {
+    return 'Минимальное число не может быть отрицательным и больше макисмального';
+  }
+};
+
+getRandom (100, 90);
+
+//проверка длины комментария
+
+const getLength = function (field, amount) {
+  if (field.length <= amount) {
+    return 'Комментарий отправлен';
+  }
+  else {
+    return 'Превышен лимит символов';
+  }
+};
+
+getLength(100, 140);
