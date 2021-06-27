fd63aa115e2cc27142543bd2d8a99ecab6a2c768
sergdeveloper <serg.developer@yandex.ru>
2021-06-28 02:17:27 +0300

    Начало модуля большого изображения

--- js/photos.js
+++ js/photos.js
@@ -13,3 +13,26 @@ userPhotos.forEach(({url, description, likes}) => {
 });
 
 photosList.appendChild(picturesFragment);
+
+const popup = document.querySelector('.big-picture');
+const preview = document.querySelectorAll('.picture');
+const likes = document.querySelectorAll('.picture__likes')
+const imgUrl = document.querySelectorAll('.picture__img')
+const imgComments = document.querySelectorAll('.picture__comments')
+const commentPicture = avatar.src;
+
+const clickOnPrev = function(prev, imgUrl, likes, imgComments, commentPicture){
+  prev.addEventListener('click', function(evt){
+    evt.preventDefault();
+    popup.classList.remove('hidden');
+    popup.querySelector('.likes-count').textContent = likes.textContent;
+    popup.querySelector('.social__caption').textContent = imgComments.textContent;
+    popup.querySelector(".big-picture__img img").src = imgUrl.src;
+    popup.querySelector(".social__picture").src = avatar.src;
+  })
+}
+
+for (let i = 0; i < preview.length; i++) {
+  clickOnPrev (preview[i], imgUrl[i], likes[i], imgComments[i], commentPicture[i]);
+}
+
