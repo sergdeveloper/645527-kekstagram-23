5cce92dee7a050087bd0510f0b6d2b9a630150d6
sergdeveloper <serg.developer@yandex.ru>
2021-07-12 21:33:50 +0300

    Загрузка по 5 комментов + разбиение на модули

---
+++ js/comment.js
@@ -0,0 +1,64 @@
+import { createNewElement } from './utils.js';
+const commentsFragment = document.createDocumentFragment();
+const bigPictureWrapper = document.querySelector('.big-picture');
+const commentsList = bigPictureWrapper.querySelector('.social__comments');
+const commentsItem = commentsList.children;
+const loadMore = document.querySelector('.social__comments-loader')
+const avatarWidth = '35';
+const avatarHeigth = '35';
+const commentsOnScreen = 5;
+const currentComments = bigPictureWrapper.querySelector('.current-count');
+//Создание одного комментария
+const createComment = () => {
+ const commentUser = createNewElement('li', 'social__comment');
+ const user = createNewElement('img', 'social__picture');
+ user.width = avatarWidth;
+ user.height = avatarHeigth;
+ commentUser.appendChild(user);
+ const textComment = createNewElement('p', 'social__text');
+ commentUser.appendChild(textComment);
+ return commentUser;
+};
+//Создание всех комментариев
+const createComments = (amount) => {
+ const commentAmount = (amount >= commentsOnScreen) ? commentsOnScreen : amount;
+ for (let i = 1; i <= commentAmount; i++) {
+   commentsFragment.appendChild(createComment());
+ }
+ commentsList.appendChild(commentsFragment);
+};
+//Добавляет контент
+const addCommnetsContent = (comments) => {
+ 
+ for (let i = 0; i < commentsItem.length; i++) {
+   const commentUser = commentsItem[i];
+   const avatar = commentUser.querySelector('.social__picture');
+   avatar.src = comments.avatar;
+   avatar.alt = comments.author;
+   const textComment = commentUser.querySelector('.social__text');
+   textComment.textContent = comments.text;
+ }
+};
+//Убрать магические числа
+const editComments = (picture) => {
+ for (let i = commentsItem.length - 1; i >= 0; i--) {
+   commentsItem[i].remove();
+ }
+ let currentLoadedComments = 5;
+ let maxComments = picture.description.length;
+ createComments(5);
+ addCommnetsContent(picture.comment);
+ loadMore.addEventListener('click', function(){
+   if (currentLoadedComments < maxComments){
+     createComments(5);
+     addCommnetsContent(picture.comment);
+     currentLoadedComments += 5;
+     const imageLoadedComments = currentLoadedComments.toString();
+     currentComments.textContent = imageLoadedComments;
+   } else {
+     loadMore.classList.add('hidden');
+   }
+ })
+};
+export {editComments};
+ 
--- js/form.js
+++ js/form.js
@@ -1,30 +1,27 @@
-import { isEscapeEvent } from './utils.js';
+import { isEscapeEvent, isEnterEvent } from './utils.js';
 import {  checkInputIsActive, hashtagInputChecker, commentInputChecker, inputHashtag, inputComment } from './validation.js';
 const body = document.querySelector('body');
-const formDownloadPicture = document.querySelector('#upload-select-image'); 
+const formDownloadPicture = document.querySelector('#upload-select-image');
 const inputFile = formDownloadPicture.querySelector('#upload-file');
 const overlayEditor = formDownloadPicture.querySelector('.img-upload__overlay');
-const buttonClose = overlayEditor.querySelector('#upload-cancel'); 
-//Обработчик нажатий на ESCAPE
-function documentdownChecker (evt) {
-  if(isEscapeEvent(evt) && !checkInputIsActive()) {
-    evt.preventDefault();
-    closePopup();
-  }
+const buttonClose = overlayEditor.querySelector('#upload-cancel');
+//Функция закрытия попапа (Обнуляет содержимое полей)
+function closePopup () {
+  overlayEditor.classList.add('hidden');
+  body.classList.remove('modal-open');
+  inputFile.value = '';
+  inputComment.value = '';
+  inputHashtag.value = '';
+  inputHashtag.removeEventListener('input', hashtagInputChecker);
+  inputComment.removeEventListener('input', commentInputChecker);
+  document.removeEventListener('keydown', documentdownChecker);
+  buttonClose.removeEventListener('keydown', buttonCloseChecker);
 }
 //Обработчик нажатий на ENTER
 function buttonCloseChecker(evt) {
   if(isEnterEvent(evt)) {
     closePopup();
   }
-}
-//Обработчик изменения загружаемого файла
-function inputFileChangeChecker () {
-  openPopup();
-}
-//Обработчик клика на "Закрыть"
-function buttonCloseClickChecker () {
-  closePopup();
 }
 //Функция открытия попапа
 function openPopup () {
@@ -36,17 +33,20 @@ function openPopup () {
   inputComment.addEventListener('input', commentInputChecker);
 }
 buttonClose.addEventListener('click', buttonCloseClickChecker);
-//Функция закрытия попапа (Обнуляет содержимое полей)
-function closePopup () {
-  overlayEditor.classList.add('hidden');
-  body.classList.remove('modal-open');
-  inputFile.value = '';
-  inputComment.value = '';
-  inputHashtag.value = '';
-  inputHashtag.removeEventListener('input', hashtagInputChecker);
-  inputComment.removeEventListener('input', commentInputChecker);
-  document.removeEventListener('keydown', documentdownChecker);
-  buttonCloseE.removeEventListener('keydown', buttonCloseChecker);
+//Обработчик нажатий на ESCAPE
+function documentdownChecker (evt) {
+  if(isEscapeEvent(evt) && !checkInputIsActive()) {
+    evt.preventDefault();
+    closePopup();
+  }
+}
+//Обработчик изменения загружаемого файла
+function inputFileChangeChecker () {
+  openPopup();
+}
+//Обработчик клика на "Закрыть"
+function buttonCloseClickChecker () {
+  closePopup();
 }
 inputFile.addEventListener('change', inputFileChangeChecker);
 
